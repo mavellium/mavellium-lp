17ac96bf0d375d782a026dc421df6c1fa3429382
Luan <mavellium@gmail.com>
2025-10-22 13:57:19 -0300

    Fix: Footer e header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ import { Icon } from "@iconify/react";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full flex flex-col justify-center items-center border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 px-30 w-full flex flex-col justify-center items-center border-b border-border bg-white">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <img src="/logo-mavellium.svg" alt="Mavellium" width={203} height={52} />
